235bdd1b1c0a64268f16f80caafa6be497bcd54e
Jake Evans <jacob.ee92@gmail.com>
2022-06-09 12:22:40 -0600

    Set achievement progress bar fill percentage

--- code/UI/Menu/Stats/StatsAchievements.cs
+++ code/UI/Menu/Stats/StatsAchievements.cs
@@ -6,93 +6,95 @@ using System.Linq;
 internal class StatsAchievements : NavigatorPanel
 {
 
-    public Panel AchievementCanvas { get; set; }
-    public string AchievementCount { get; set; }
+	public Panel AchievementProgressBar { get; set; }
+	public Panel AchievementCanvas { get; set; }
+	public string AchievementCount { get; set; }
 
-    private void RebuildAchievements()
-    {
-        // setting dummy achievements to debug/preview
-        Achievement.Set(Local.PlayerId, "uf_dummy_complete");
-        Achievement.Set(Local.PlayerId, "uf_dummy_climb_complete", "willow.uf_climb");
+	private void RebuildAchievements()
+	{
+		// setting dummy achievements to debug/preview
+		Achievement.Set( Local.PlayerId, "uf_dummy_complete" );
+		Achievement.Set( Local.PlayerId, "uf_dummy_climb_complete", "willow.uf_climb" );
 
-        AchievementCanvas.DeleteChildren(true);
+		AchievementCanvas.DeleteChildren( true );
 
-        var mapAchievements = Achievement.FetchForMap();
-        var total = 0;
-        var achieved = 0;
+		var mapAchievements = Achievement.FetchForMap();
+		var total = 0;
+		var achieved = 0;
 
-        foreach (var ach in mapAchievements)
-        {
-            if (IsMedal(ach))
-            {
-                // let's hide medals if the map hasn't defined time thresholds
-                if (!Entity.All.Any(x => x is AchievementMedals))
-                    continue;
+		foreach ( var ach in mapAchievements )
+		{
+			if ( IsMedal( ach ) )
+			{
+				// let's hide medals if the map hasn't defined time thresholds
+				if ( !Entity.All.Any( x => x is AchievementMedals ) )
+					continue;
 
-                // hard-coding medal times into the description
-                ach.Description = GetMedalDescription(ach);
-            }
+				// hard-coding medal times into the description
+				ach.Description = GetMedalDescription( ach );
+			}
 
-            var entry = new StatsAchievementsEntry(ach);
-            entry.Parent = AchievementCanvas;
+			var entry = new StatsAchievementsEntry( ach );
+			entry.Parent = AchievementCanvas;
 
-            // we can add +30xp or whatever to the icon if desired
-            var xpGranted = ExperienceGranted(ach);
-            if (xpGranted > 0)
-            {
+			// we can add +30xp or whatever to the icon if desired
+			var xpGranted = ExperienceGranted( ach );
+			if ( xpGranted > 0 )
+			{
 
-            }
+			}
 
-            if (ach.IsCompleted())
-            {
-                achieved++;
-            }
+			if ( ach.IsCompleted() )
+			{
+				achieved++;
+			}
 
-            total++;
-        }
+			total++;
+		}
 
-        AchievementCount = $"{achieved}/{total} Earned";
-    }
+		AchievementCount = $"{achieved}/{total} Earned";
+		AchievementProgressBar.Style.Width = Length.Percent( ((float)achieved / total) * 100f );
+	}
 
-    private static bool IsMedal(Achievement ach)
-    {
-        return new string[]
-        {
-            "uf_bronze",
-            "uf_silver",
-            "uf_gold"
-        }.Contains(ach.ShortName);
-    }
+	private static bool IsMedal( Achievement ach )
+	{
+		return new string[]
+		{
+			"uf_bronze",
+			"uf_silver",
+			"uf_gold"
+		}.Contains( ach.ShortName );
+	}
 
-    private static int ExperienceGranted(Achievement ach)
-    {
-        var pass = TrailPass.Current;
-        var tpAchi = pass.Achievements.FirstOrDefault(x => x.AchievementShortName == ach.ShortName);
-        if (tpAchi == null) return 0;
+	private static int ExperienceGranted( Achievement ach )
+	{
+		var pass = TrailPass.Current;
+		var tpAchi = pass.Achievements.FirstOrDefault( x => x.AchievementShortName == ach.ShortName );
+		if ( tpAchi == null ) return 0;
 
-        return tpAchi.ExperienceGranted;
-    }
+		return tpAchi.ExperienceGranted;
+	}
 
-    private static string GetMedalDescription(Achievement ach)
-    {
-        var achMedals = Entity.All.FirstOrDefault(x => x is AchievementMedals) as AchievementMedals;
-        if (!achMedals.IsValid()) return ach.Description;
+	private static string GetMedalDescription( Achievement ach )
+	{
+		var achMedals = Entity.All.FirstOrDefault( x => x is AchievementMedals ) as AchievementMedals;
+		if ( !achMedals.IsValid() ) return ach.Description;
 
-        var time = ach.ShortName switch
-        {
-            "uf_bronze" => achMedals.Bronze,
-            "uf_silver" => achMedals.Silver,
-            "uf_gold" => achMedals.Gold,
-            _ => 0
-        };
+		var time = ach.ShortName switch
+		{
+			"uf_bronze" => achMedals.Bronze,
+			"uf_silver" => achMedals.Silver,
+			"uf_gold" => achMedals.Gold,
+			_ => 0
+		};
 
-        return $"Complete the map in {CourseTimer.FormattedTimeMs(time)}s or better";
-    }
+		return $"Complete the map in {CourseTimer.FormattedTimeMs( time )}s or better";
+	}
 
-    [Event.Entity.PostSpawn]
-    private void PostEntitiesSpawned() => RebuildAchievements();
-    public override void OnHotloaded() => RebuildAchievements();
-    protected override void PostTemplateApplied() => RebuildAchievements();
+	[Event.Entity.PostSpawn]
+	private void PostEntitiesSpawned() => RebuildAchievements();
+	public override void OnHotloaded() => RebuildAchievements();
+	protected override void PostTemplateApplied() => RebuildAchievements();
 
 }
 
