b991aed32ced6edbd0f573106a4b0484bcdcb3f1
Jake Evans <jacob.ee92@gmail.com>
2022-06-09 12:44:23 -0600

    Display how much xp an achievement grants

--- code/UI/Menu/Stats/StatsAchievementsEntry.cs
+++ code/UI/Menu/Stats/StatsAchievementsEntry.cs
@@ -1,5 +1,6 @@
 ﻿
 using Sandbox.UI;
+using System.Linq;
 
 [UseTemplate]
 internal class StatsAchievementsEntry : Panel
@@ -7,11 +8,23 @@ internal class StatsAchievementsEntry : Panel
 
 	public Achievement Achievement { get; set; }
 	public Panel Icon { get; set; }
+	public int Experience { get; set; }
 
 	public StatsAchievementsEntry( Achievement achievement )
 	{
 		Achievement = achievement;
 		Icon.Style.SetBackgroundImage( achievement.Thumbnail );
+
+
+		var tpa = TrailPass.Current.Achievements.FirstOrDefault( x => x.AchievementShortName == Achievement.ShortName );
+		if ( tpa == null || tpa.ExperienceGranted == 0 )
+		{
+			SetClass( "grantsxp", false );
+			return;
+		}
+
+		SetClass( "grantsxp", true );
+		Experience = tpa.ExperienceGranted;
 	}
 
 	public override void Tick()
