5f66d8d7186561d78c9721a101c9106d4270c910
Jake Evans <jacob.ee92@gmail.com>
2022-09-19 15:54:08 -0600

    Fill in achievements tab with available maps

---
+++ code/UI/Menu/Achievements/AchievementsMapThumb.cs
@@ -0,0 +1,43 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+
+[UseTemplate]
+internal class AchievementsMapThumb : Panel
+{
+
+	private Package Package;
+
+	public string MapName => FriendlyName();
+	public Panel MapImage { get; protected set; }
+
+	public AchievementsMapThumb() { }
+
+	public AchievementsMapThumb( Package package )
+	{
+		Package = package;
+
+		Rebuild();
+	}
+
+	private void Rebuild()
+	{
+		if ( Package == null ) 
+			return;
+
+		MapImage.Style.SetBackgroundImage( Package.Thumb );
+	}
+
+	private string FriendlyName()
+	{
+		if ( Package == null ) 
+			return "Unknown";
+
+		var result = Package.Title;
+		if ( result.StartsWith( "unicycle frenzy", System.StringComparison.OrdinalIgnoreCase ) )
+			result = result[15..].Trim();
+
+		return result;
+	}
+
+}
