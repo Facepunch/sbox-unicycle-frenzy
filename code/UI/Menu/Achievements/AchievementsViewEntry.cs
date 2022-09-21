3106ab083c617ed1df1e44e39d18e6b3e2bd12ac
Jake Evans <jacob.ee92@gmail.com>
2022-09-21 11:33:56 -0600

    Load achievement data into entry

--- code/UI/Menu/Achievements/AchievementsViewEntry.cs
+++ code/UI/Menu/Achievements/AchievementsViewEntry.cs
@@ -1,4 +1,5 @@
 ﻿
+using Sandbox;
 using Sandbox.UI;
 
 [UseTemplate]
@@ -6,13 +7,31 @@ internal class AchievementsViewEntry : Panel
 {
 
 	public Achievement Achievement { get; }
+	public Panel Thumbnail { get; protected set; }
 
 	public AchievementsViewEntry() { }
 	public AchievementsViewEntry( Achievement achievement ) 
 	{
 		Achievement = achievement;
+
+		Rebuild();
 	}
 
+	protected override void PostTemplateApplied()
+	{
+		base.PostTemplateApplied();
 
+		Rebuild();
+	}
+
+	[Event.Hotload]
+	private void Rebuild() 
+	{
+		if ( Achievement == null ) 
+			return;
+
+		SetClass( "is-completed", Achievement.IsCompleted() );
+		Thumbnail.Style.SetBackgroundImage( Achievement.Thumbnail );
+	}
 
 }
