d67581833055d3fd8970699b3a7915f590674e5a
Jake Evans <jacob.ee92@gmail.com>
2022-09-22 21:08:23 -0600

    Prematch ui & countdown sfx

---
+++ code/UI/PreMatchHud.cs
@@ -0,0 +1,49 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+using System;
+
+[UseTemplate]
+internal class PreMatchHud : Panel
+{
+
+	public int TimeLeft { get; set; } = 20;
+
+	private TimeSince TimeSincePulse;
+
+	[Event.Frame]
+	private void OnFrame()
+	{
+		base.Tick();
+
+		if ( UnicycleFrenzy.Game.GameState != UnicycleFrenzy.GameStates.PreMatch )
+		{
+			SetClass( "open", false );
+			return;
+		}
+
+		SetClass( "open", true );
+
+		var timeleft = MathX.CeilToInt( UnicycleFrenzy.Game.StateTimer );
+		if( timeleft < TimeLeft )
+		{
+			TimeLeft = timeleft;
+			Pulse();
+		}
+
+		if ( TimeSincePulse > .25f )
+		{
+			RemoveClass( "pulse" );
+		}
+	}
+
+	private void Pulse()
+	{
+		TimeSincePulse = 0;
+		SetClass( "pulse", true );
+
+		if( TimeLeft >= 0 )
+			Sound.FromScreen( "sounds/ui/timer.prematch.pulse.sound" );
+	}
+
+}
