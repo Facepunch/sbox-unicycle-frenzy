9b886211db83c567d111c7ee326a518f42549bd5
bakscratch <lt_8@hotmail.co.uk>
2022-10-07 17:47:02 +0100

    updated lean hud style

--- code/ui/UnicycleLean.cs
+++ code/ui/UnicycleLean.cs
@@ -8,16 +8,23 @@ internal class UnicycleLean : Panel
 	public Label LeftLean;
 
 	public Label RightLean;
+	
+	public Label CenterLean;
+	
+	public Label PointLean;
+
+	public Panel UFLean { get; set; }
+	public Panel AbsLean { get; set; }
+	public Panel LocalLean { get; set; }
+	public Panel SafeZone { get; set; }
 
 	public static UnicycleLean Current { get; private set; }
 
 	public UnicycleLean()
 	{
 		Current = this;
-		LeftLean = Add.Label( "", "leftlean" );
-		RightLean = Add.Label( "", "rightlean" );
 	}
-
+	
 	public override void Tick()
 	{
 		if ( Local.Pawn is not UnicyclePlayer player ) return;
@@ -25,12 +32,26 @@ internal class UnicycleLean : Panel
 
 		var maxLean = controller.MaxLean;
 
+		//var localLean = player.Tilt;
+		//var LeftRollAlpha = localLean.roll.LerpInverse( -maxLean, maxLean );
+		//var RightRollAlpha = localLean.roll.LerpInverse( maxLean, -maxLean );
+
+		var absLean = player.Rotation.Angles();
+		var absRollAlpha = absLean.roll.LerpInverse( -maxLean, maxLean );
+		var absPitchAlpha = absLean.pitch.LerpInverse( maxLean, -maxLean );
+
+		AbsLean.Style.Left = Length.Percent( absRollAlpha * 100f );
+		AbsLean.Style.Top = Length.Percent( absPitchAlpha * 100f );
+
+
 		var localLean = player.Tilt;
-		var LeftRollAlpha = localLean.roll.LerpInverse( -maxLean, maxLean );
-		var RightRollAlpha = localLean.roll.LerpInverse( maxLean, -maxLean );
+		var localRollAlpha = localLean.roll.LerpInverse( -maxLean, maxLean );
+		var localPitchAlpha = localLean.pitch.LerpInverse( maxLean, -maxLean );
 
+		LocalLean.Style.Left = Length.Percent( localRollAlpha * 100f );
+		LocalLean.Style.Top = Length.Percent( localPitchAlpha * 100f );
 
-		LeftLean.Style.Opacity = MathX.Lerp( .75f, -1, LeftRollAlpha );
-		RightLean.Style.Opacity = MathX.Lerp( .75f, -1, RightRollAlpha );
+		//LeftLean.Style.Opacity = MathX.Lerp( .75f, -1, LeftRollAlpha );
+		//RightLean.Style.Opacity = MathX.Lerp( .75f, -1, RightRollAlpha );
 	}
 }
