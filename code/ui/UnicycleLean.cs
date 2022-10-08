eaf0a63d5aabb917def9df2a42e6b5a8f9b91466
Jake Evans <jacob.ee92@gmail.com>
2022-10-08 10:36:41 -0600

    Lean hud cleanup

--- code/ui/UnicycleLean.cs
+++ code/ui/UnicycleLean.cs
@@ -5,25 +5,9 @@ using Sandbox.UI.Construct;
 [UseTemplate]
 internal class UnicycleLean : Panel
 {
-	public Label LeftLean;
 
-	public Label RightLean;
-	
-	public Label CenterLean;
-	
-	public Label PointLean;
-
-	public Panel UFLean { get; set; }
 	public Panel AbsLean { get; set; }
 	public Panel LocalLean { get; set; }
-	public Panel SafeZone { get; set; }
-
-	public static UnicycleLean Current { get; private set; }
-
-	public UnicycleLean()
-	{
-		Current = this;
-	}
 	
 	public override void Tick()
 	{
@@ -32,10 +16,6 @@ internal class UnicycleLean : Panel
 
 		var maxLean = controller.MaxLean;
 
-		//var localLean = player.Tilt;
-		//var LeftRollAlpha = localLean.roll.LerpInverse( -maxLean, maxLean );
-		//var RightRollAlpha = localLean.roll.LerpInverse( maxLean, -maxLean );
-
 		var absLean = player.Rotation.Angles();
 		var absRollAlpha = absLean.roll.LerpInverse( -maxLean, maxLean );
 		var absPitchAlpha = absLean.pitch.LerpInverse( maxLean, -maxLean );
@@ -43,15 +23,11 @@ internal class UnicycleLean : Panel
 		AbsLean.Style.Left = Length.Percent( absRollAlpha * 100f );
 		AbsLean.Style.Top = Length.Percent( absPitchAlpha * 100f );
 
-
 		var localLean = player.Tilt;
 		var localRollAlpha = localLean.roll.LerpInverse( -maxLean, maxLean );
 		var localPitchAlpha = localLean.pitch.LerpInverse( maxLean, -maxLean );
 
 		LocalLean.Style.Left = Length.Percent( localRollAlpha * 100f );
 		LocalLean.Style.Top = Length.Percent( localPitchAlpha * 100f );
-
-		//LeftLean.Style.Opacity = MathX.Lerp( .75f, -1, LeftRollAlpha );
-		//RightLean.Style.Opacity = MathX.Lerp( .75f, -1, RightRollAlpha );
 	}
 }
