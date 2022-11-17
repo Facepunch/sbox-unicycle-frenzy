d5390008edda18c277cc4e77f1347fb1acceb4e9
Jake Evans <jacob.ee92@gmail.com>
2022-11-16 20:29:16 -0600

    Convert stats tab, map info, and achievement list to razor

--- code/UI/Components/ProgressBar.cs
+++ code/UI/Components/ProgressBar.cs
@@ -8,10 +8,15 @@ internal class ProgressBar : Panel
 	public Panel Bar { get; set; }
 	public Label Title { get; set; }
 
+	public int Current { get; set; }
+	public int Max { get; set; }
+
 	public void Set( int current, int max )
 	{
-		Title.Text = $"{current}/{max}";
-		Bar.Style.Width = Length.Fraction( current / (float)max );
+		Current = current;
+		Max = max;
+		//Title.Text = $"{current}/{max}";
+		//Bar.Style.Width = Length.Fraction( current / (float)max );
 	}
 
 }
