2f0494b819301765127ece6e660f82987f1620a4
bakscratch <lt_8@hotmail.co.uk>
2024-04-23 14:39:26 +0100

    Wip unicycle customiser

--- code/Player/UFItems.cs
+++ code/Player/UFItems.cs
@@ -3,16 +3,27 @@ public class UnicycleFrenzyItems : GameResource
 {
 	public string ItemName { get; set; }
 	public string ItemDescription { get; set; }
-	[ImageAssetPath]public string ItemImage { get; set; }
+	[ImageAssetPath] public string ItemImage { get; set; } = "textures/ui/map-thumbnail-placeholder.png";
 	public Model ItemModel { get; set; }
 	public ItemCategories ItemCategory { get; set; } = ItemCategories.Frame;
 	public ItemRare ItemRarity { get; set; } = ItemRare.Common;
+}
+
+public class UnicycleDressed
+{
+	public UnicycleFrenzyItems Frame { get; set; }
+	public UnicycleFrenzyItems Seat { get; set; }
+	public UnicycleFrenzyItems Wheel { get; set; }
+	public UnicycleFrenzyItems Accessory { get; set; }
+	public UnicycleFrenzyItems Pedal { get; set; }
 }
 
 public enum ItemCategories
 {
 	Frame,
 	Wheel,
+	Seat,
+	Pedal,
 	Accessory
 }
 
@@ -23,4 +34,42 @@ public enum ItemRare
 	Rare,
 	Epic,
 	Legendary
+}
+
+[Title( "Unicycle Dresser" )]
+[Category( "Unicycle Frenzy" )]
+[Icon( "shopping_cart" )]
+internal class UnicycleDresser : Component
+{
+	[Property] ModelRenderer Frame { get; set; }	
+	[Property] ModelRenderer Seat { get; set; }	
+	[Property] ModelRenderer Wheel { get; set; }
+	//[Property] ModelRenderer Accessory { get; set; }
+	[Property] ModelRenderer LeftPedal { get; set; }
+	[Property] ModelRenderer RightPedal { get; set; }
+
+	public static UnicycleDressed Local;
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+
+		Local = FileSystem.Data.ReadJson<UnicycleDressed>( "unicycle.dress.json" );
+
+		if ( Local != null )
+		{
+			Frame.Model = Local.Frame.ItemModel;
+			Seat.Model = Local.Seat.ItemModel;
+			Wheel.Model = Local.Wheel.ItemModel;
+			//Accessory.Model = Local.Accessory.ItemModel;
+			LeftPedal.Model = Local.Pedal.ItemModel;
+			RightPedal.Model = Local.Pedal.ItemModel;
+		}
+	}
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+
+	}
 }
