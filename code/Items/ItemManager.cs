3cbb465b96614ff15fbd0acf5a1b82a467cff0b1
bakscratch <lt_8@hotmail.co.uk>
2024-04-25 16:10:18 +0100

    Trail pass system

---
+++ code/Items/ItemManager.cs
@@ -0,0 +1,57 @@
+using Sandbox;
+
+public sealed class ItemManager : Component
+{
+	UnicycleProgression Progression { get; set; }
+	UFItemProgression ProgressionResource { get; set; }
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+
+		ProgressionResource = ResourceLibrary.GetAll<UFItemProgression>().Where( x => x.IsCurrentPass ).FirstOrDefault();
+	}
+
+	protected override void OnUpdate()
+	{
+		Fetch();
+
+		if ( ProgressionResource != null )
+		{
+			GetProgress();
+		}
+		//Items = Progression.UnlockedItems;
+	}
+
+	public void GetProgress()
+	{
+		foreach ( var item in ProgressionResource.ItemsInPass )
+		{
+			Log.Info( item.Item.ItemName );
+
+			if( Progression.UnlockedItems.Contains( item.Item ) ) continue;
+			if( Progression.CurrentXP < item.XPNeeded ) continue;
+			Progression.UnlockedItems.Add( item.Item );
+
+			Save();
+		}
+	}
+
+	void Fetch()
+	{
+		Progression = FileSystem.Data.ReadJson<UnicycleProgression>( "unicycle.progression.json" );
+		if ( Progression == null )
+		{
+			Progression = new UnicycleProgression();
+		}
+	}
+
+	void Save()
+	{
+		if ( Progression == null )
+		{
+			Fetch();
+		}
+		FileSystem.Data.WriteJson( "unicycle.progression.json", Progression );
+	}
+}
---
+++ code/Items/UFItemProgression.cs
@@ -0,0 +1,28 @@
+﻿[GameResource( "Unicycle Frenzy Item Pass", "ufpass", "UnicycleFrenzyItemPass", Icon = "airplane_ticket", IconBgColor = "#2e1236", IconFgColor = "#f0f0f0" )]
+public class UFItemProgression : GameResource
+{
+	//Add the default ones
+	public List<ItemProgress> ItemsInPass { get; set; } = new List<ItemProgress> {
+		new ItemProgress { Item = ResourceLibrary.Get<UnicycleFrenzyItems>( "resources/items/defaultframe.ufitem" ), XPNeeded = 0, ShowInPass = false, DefaultItem = true },
+		new ItemProgress { Item = ResourceLibrary.Get<UnicycleFrenzyItems>( "resources/items/defaultseat.ufitem" ), XPNeeded = 0, ShowInPass = false, DefaultItem = true },
+		new ItemProgress { Item = ResourceLibrary.Get<UnicycleFrenzyItems>( "resources/items/defaultwheel.ufitem" ), XPNeeded = 0, ShowInPass = false , DefaultItem = true},
+		new ItemProgress { Item =  ResourceLibrary.Get<UnicycleFrenzyItems>( "resources/items/defaultpedal.ufitem" ), XPNeeded = 0, ShowInPass = false, DefaultItem = true }
+		};
+	public float MaxSeasonXP { get; set; }
+	public bool IsCurrentPass { get; set; }
+
+	public struct ItemProgress
+	{
+		public UnicycleFrenzyItems Item { get; set; }
+		public float XPNeeded { get; set; }
+		[Hide]public bool IsUnlocked { get; set; }
+		public bool ShowInPass { get; set; }
+		public bool DefaultItem { get; set; }
+	}
+}
+
+public class UnicycleProgression
+{
+	public float CurrentXP { get; set; }
+	public List<UnicycleFrenzyItems> UnlockedItems { get; set; } = new List<UnicycleFrenzyItems>();
+}
